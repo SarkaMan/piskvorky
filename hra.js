90001a2929f5d0f8bccb27908bbab4039942fc54
Šárka Manďáková <sarka.mandakova@gmail.com>
2024-04-16 21:24:47 +0200

    Oživeno všech 100 políček

--- hra.js
+++ hra.js
@@ -1,3 +1,5 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
 
 const selectButton = (evt) => {
@@ -12,38 +14,57 @@ const selectButton = (evt) => {
     currentPlayer = 'circle';
     document.getElementById('current__player').src = 'circle.svg';
   }
+  result();
+  console.log(createArray());
+};
+//Posluchač událostí pro všechna tlačítka
+const btn = document.querySelectorAll('.board__button');
+btn.forEach((button) => {
+  button.addEventListener('click', selectButton);
+});
+
+//Vytvoření pole
+const createArray = () => {
+  let array = [];
+  btn.forEach((button) => {
+    if (button.classList.contains('board__field--circle')) {
+      array.push('o');
+    } else if (button.classList.contains('board__field--cross')) {
+      array.push('x');
+    } else {
+      array.push('_');
+    }
+  });
+  return array;
 };
 
-document
-  .querySelector('button:nth-child(1)')
-  .addEventListener('click', selectButton);
-document
-  .querySelector('button:nth-child(2)')
-  .addEventListener('click', selectButton);
-document
-  .querySelector('button:nth-child(3)')
-  .addEventListener('click', selectButton);
-document
-  .querySelector('button:nth-child(4)')
-  .addEventListener('click', selectButton);
-document
-  .querySelector('button:nth-child(5)')
-  .addEventListener('click', selectButton);
-document
-  .querySelector('button:nth-child(6)')
-  .addEventListener('click', selectButton);
-document
-  .querySelector('button:nth-child(7)')
-  .addEventListener('click', selectButton);
-document
-  .querySelector('button:nth-child(8)')
-  .addEventListener('click', selectButton);
-document
-  .querySelector('button:nth-child(9)')
-  .addEventListener('click', selectButton);
-document
-  .querySelector('button:nth-child(10)')
-  .addEventListener('click', selectButton);
+//Určení vítěze a nastavení časovače
+const result = () => {
+  let array = createArray();
+
+  const winner = findWinner(array);
+
+  if (winner === 'o' || winner === 'x') {
+    setTimeout(() => {
+      alert(`Vítězem je hráč se symbolem ${winner}.`);
+    }, 1000);
+    setTimeout(() => {
+      location.reload();
+    }, 2000);
+    return false;
+  }
+
+  if (winner === 'tie') {
+    setTimeout(() => {
+      alert(`Hra skončila remízou.`);
+    }, 1000);
+    setTimeout(() => {
+      location.reload();
+    }, 2000);
+    return false;
+  }
+  return true;
+};
 
 //Ošetření situace, kdy uživatel klikne omylem na tlačítko restart
 document
