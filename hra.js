4b07d73d298a2a6935fe7674480ca179335e0d60
Šárka Manďáková <sarka.mandakova@gmail.com>
2024-04-25 10:12:53 +0200

    Zapojení AI do hry

--- hra.js
+++ hra.js
@@ -1,32 +1,70 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 let currentPlayer = 'circle';
+const allButtons = document.querySelectorAll('.board__button');
 
+//Funkce apiPlayer ziskava aktualni stav hraci desky, ktery odesila na API a vraci tah AI
+const apiPlayer = async (player) => {
+  const playingBoard = [];
+
+  allButtons.forEach((button) => {
+    if (button.classList.contains('board__field--circle')) {
+      playingBoard.push('o');
+    } else if (button.classList.contains('board__field--cross')) {
+      playingBoard.push('x');
+    } else {
+      playingBoard.push('_');
+    }
+  });
+
+  //Odeslani pozadavku fetch
+  const response = await fetch(
+    'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        board: playingBoard,
+        player: player,
+      }),
+    },
+  );
+
+  //Ziskani dat z odpovedi
+  const data = await response.json();
+  const { x, y } = data.position;
+  const fieldIndex = x + y * 10; //najde policko na prislusne pozici
+  const field = allButtons[fieldIndex];
+  field.click(); //simulace kliknuti na tlacitko - odehraje AI
+};
+
+//Funkce, ktera je volana pri kliknuti na tlacitko hraci desky
 const selectButton = (evt) => {
   const btn = evt.target;
   evt.target.disabled = true;
   if (currentPlayer === 'circle') {
     btn.classList.add('board__field--circle');
-    currentPlayer = 'cross';
     document.getElementById('current__player').src = 'cross.svg';
+    currentPlayer = 'cross';
+    apiPlayer('x');
   } else {
     btn.classList.add('board__field--cross');
-    currentPlayer = 'circle';
     document.getElementById('current__player').src = 'circle.svg';
+    currentPlayer = 'circle';
   }
   result();
-  console.log(createArray());
 };
-//Posluchač událostí pro všechna tlačítka
-const btn = document.querySelectorAll('.board__button');
-btn.forEach((button) => {
+
+allButtons.forEach((button) => {
   button.addEventListener('click', selectButton);
 });
 
-//Vytvoření pole
+//Vytvoreni pole
 const createArray = () => {
-  let array = [];
-  btn.forEach((button) => {
+  const array = [];
+  allButtons.forEach((button) => {
     if (button.classList.contains('board__field--circle')) {
       array.push('o');
     } else if (button.classList.contains('board__field--cross')) {
@@ -38,35 +76,27 @@ const createArray = () => {
   return array;
 };
 
-//Určení vítěze a nastavení časovače
+//Urceni viteze hry a nastaveni casovace
 const result = () => {
-  let array = createArray();
-
+  const array = createArray();
   const winner = findWinner(array);
 
   if (winner === 'o' || winner === 'x') {
     setTimeout(() => {
       alert(`Vítězem je hráč se symbolem ${winner}.`);
-    }, 1000);
-    setTimeout(() => {
       location.reload();
-    }, 2000);
-    return false;
+    }, 1000);
   }
 
   if (winner === 'tie') {
     setTimeout(() => {
       alert(`Hra skončila remízou.`);
-    }, 1000);
-    setTimeout(() => {
       location.reload();
-    }, 2000);
-    return false;
+    }, 1000);
   }
-  return true;
 };
 
-//Ošetření situace, kdy uživatel klikne omylem na tlačítko restart
+//Tlacitko restart
 document
   .querySelector('.button__restart')
   .addEventListener('click', (event) => {
